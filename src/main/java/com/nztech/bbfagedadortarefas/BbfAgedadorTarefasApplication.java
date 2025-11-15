e7e332518f395742955fbe7a675a91172df2ccc1
1Emanuel <emanuelfrans327@gmail.com>
2025-11-15 16:54:56 +0200

    commit inicail criar esqueleto

---
+++ src/main/java/com/nztech/bbfagedadortarefas/BbfAgedadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.nztech.bbfagedadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbfAgedadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbfAgedadorTarefasApplication.class, args);
+	}
+
+}
